5674631ea3eb867ec2bfc98b4179b1b712e7c4ff
sjq <sjq@admins-mac-mini.local>
2017-05-05 16:51:13 +0800

    Initial Commit

---
+++ ShareBicycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareBicycle
+//
+//  Created by sjq on 17/5/5.
+//  Copyright © 2017年 sjq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareBicycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareBicycle
+//
+//  Created by sjq on 17/5/5.
+//  Copyright © 2017年 sjq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
